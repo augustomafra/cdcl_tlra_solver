228f6d7c1994b8199de4f53dbd3e6b6741df2843
Augusto Mafra <augustomafra1@gmail.com>
2024-12-08 18:26:54 -0300

    Use argparse to parse command line

--- cdcl_tlra_solver.py
+++ cdcl_tlra_solver.py
@@ -2,27 +2,38 @@ import pysmt.shortcuts
 import pysmt.smtlib.parser
 import pysmt.smtlib.script
 
-import sys
+import argparse
 
-smt_parser = pysmt.smtlib.parser.SmtLibParser()
-smt_lib2_file = sys.argv[1]
-script = smt_parser.get_script_fname(smt_lib2_file)
+def eval_smt_lib2_script(script, solver):
+    steps = []
+    for command in script.commands:
+        try:
+            result = pysmt.smtlib.script.evaluate_command(command, solver)
+            steps.append((command.name, result))
+        except RuntimeError as error:
+            print("ERROR: {}: {}".format(command.name, error))
+            steps.append((command.name, None))
 
-solver = pysmt.shortcuts.Solver(name="cvc5")
+    for command, result in steps:
+        match command:
+            case "check-sat":
+                print("sat" if result else "unsat")
+            case "get-model":
+                if result:
+                    print(result)
 
-steps = []
-for command in script.commands:
-    try:
-        result = pysmt.smtlib.script.evaluate_command(command, solver)
-        steps.append((command.name, result))
-    except RuntimeError as error:
-        print("ERROR: {}: {}".format(command.name, error))
-        steps.append((command.name, None))
+def main():
+    arg_parser = argparse.ArgumentParser(prog="cdcl_tlra_solver",
+                                         description="A Python CDCL(TLRA) SMT solver")
+    arg_parser.add_argument("smt_lib2_filename")
+    args = arg_parser.parse_args()
 
-for command, result in steps:
-    match command:
-        case "check-sat":
-            print("sat" if result else "unsat")
-        case "get-model":
-            if result:
-                print(result)
+    smt_parser = pysmt.smtlib.parser.SmtLibParser()
+    script = smt_parser.get_script_fname(args.smt_lib2_filename)
+
+    solver = pysmt.shortcuts.Solver(name="cvc5")
+
+    eval_smt_lib2_script(script, solver)
+
+if __name__ == "__main__":
+    main()
