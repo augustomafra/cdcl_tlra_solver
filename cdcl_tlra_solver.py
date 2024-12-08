d5178395458d98a56c6f6f4070f50fac69995e1c
Augusto Mafra <augustomafra1@gmail.com>
2024-12-08 19:39:24 -0300

    Implement BoolAbstraction and basic clausifier

--- cdcl_tlra_solver.py
+++ cdcl_tlra_solver.py
@@ -1,3 +1,6 @@
+import pysat.formula
+import pysmt.operators
+import pysat.solvers
 import pysmt.shortcuts
 import pysmt.smtlib.parser
 import pysmt.smtlib.script
@@ -22,6 +25,73 @@ def eval_smt_lib2_script(script, solver):
                 if result:
                     print(result)
 
+class BooleanAbstraction():
+    def __init__(self, formula):
+        """ TODO: Do conversion to CNF.
+            For now we assume the input formula is CNF
+        """
+        self.formula = formula
+        self.atoms = list()
+        self.abstractions = dict()
+        self.clauses = list()
+        for atom in formula.get_atoms():
+            if atom not in self.abstractions:
+                self.atoms.append(atom)
+                self.abstractions[atom] = len(self.atoms)
+
+    def get_abstraction(self, expr):
+        return self.abstractions[expr] if expr in self.abstractions else None
+
+    def get_atom(self, abstraction):
+        return self.atoms[abstraction - 1]
+
+    def get_clauses(self):
+        if self.clauses:
+            return self.clauses
+
+        for expr in self.formula.args():
+            clause = self.clausify(expr)
+            self.clauses.append(clause)
+
+        return self.clauses
+
+    def clausify(self, expr):
+        abstraction = self.get_abstraction(expr)
+        if abstraction:
+            # Expr is an atom, so simply return a clause with its abstraction
+            return [abstraction]
+
+        match expr.node_type():
+            case pysmt.operators.NOT:
+                subexpr = self.clausify(expr.arg(0))
+                if len(subexpr) != 1:
+                    raise NotImplementedError("Clausifier does not support NOT operation with more than one operand")
+                return [-subexpr[0]]
+
+            case pysmt.operators.OR:
+                lhs = self.clausify(expr.arg(0))
+                if len(lhs) != 1:
+                    raise NotImplementedError("Clausifier does not support OR operation with more than one level of nesting")
+                rhs = self.clausify(expr.arg(1))
+                if len(rhs) != 1:
+                    raise NotImplementedError("Clausifier does not support OR operation with more than one level of nesting")
+                return [lhs[0], rhs[0]]
+
+            case pysmt.operators.AND:
+                raise NotImplementedError("Clausifier does not support AND operation, assuming CNF input for now")
+
+        raise NotImplementedError()
+
+def get_sat_assignment(clauses):
+    cnf = pysat.formula.CNF(from_clauses=clauses)
+    with pysat.solvers.Solver(name="minisat22", bootstrap_with=cnf) as solver:
+        if solver.solve():
+            print("sat")
+            return solver.get_model()
+        else:
+            print("unsat")
+    return []
+
 def main():
     arg_parser = argparse.ArgumentParser(prog="cdcl_tlra_solver",
                                          description="A Python CDCL(TLRA) SMT solver")
@@ -34,6 +104,16 @@ def main():
     solver = pysmt.shortcuts.Solver(name="cvc5")
 
     eval_smt_lib2_script(script, solver)
+
+    bool_abstraction = BooleanAbstraction(script.get_strict_formula())
+    print(bool_abstraction.abstractions)
+
+    clauses = bool_abstraction.get_clauses()
+    print(clauses)
+
+    assignment = get_sat_assignment(clauses)
+    for abstraction in assignment:
+        print(bool_abstraction.get_atom(abstraction))
 
 if __name__ == "__main__":
     main()
