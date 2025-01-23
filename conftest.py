00a4690946d071cacfbd3646523060e186d7036b
Augusto Mafra <augustomafra1@gmail.com>
2025-01-23 20:45:35 -0300

    Remove pytest framework for running benchmarks

--- conftest.py
+++
@@ -1,6 +0,0 @@
-collect_ignore_glob = [
-    "tests/benchmark/non-incremental/QF_LRA/2017-Heizmann-UltimateInvariantSynthesis/test__sorting_bubblesort_ground_i_4_3_3_bpl_11.py",
-    "tests/benchmark/non-incremental/QF_LRA/DTP-Scheduling/test_constraints_tempo_width_50.py",
-    "tests/benchmark/non-incremental/QF_LRA/LassoRanker/CooperatingT2/test_afagp_fail_t2_c_Iteration1_Lasso_7_phaseTemplate.py",
-    "tests/benchmark/non-incremental/QF_LRA/LassoRanker/CooperatingT2/test_brp_withassume_t2_c_Iteration1_Loop_7_phaseTemplate.py"
-]
