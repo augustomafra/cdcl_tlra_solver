84daa1f2169e39ff8e5e6f02e8660adffba549c2
Augusto Mafra <augustomafra1@gmail.com>
2025-01-24 00:02:33 -0300

    Report exit status on errors

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -3,7 +3,13 @@ time_limit="$1"
 benchmark="$2"
 output_log="$3"
 timeout "${time_limit}" python3 ../cdcl_tlra_solver.py "${benchmark}" 2>&1 | tee "${output_log}"
-if [ "${PIPESTATUS[0]}" = 124 ]
+status="${PIPESTATUS[0]}"
+if [ "${status}" = 124 ]
 then
     echo "error: timed out after ${time_limit}" | tee -a "${output_log}"
+    exit
+fi
+if [ "${status}" != 0 ]
+then
+    echo "error: exit code ${status}" | tee -a "${output_log}"
 fi
