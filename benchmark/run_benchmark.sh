fe46ba571ac579ee38c675f2bae91eca91fbbd58
Augusto Mafra <augustomafra1@gmail.com>
2025-01-26 16:56:42 -0300

    Allow running tests with different solver

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -2,7 +2,8 @@
 time_limit="$1"
 benchmark="$2"
 output_log="$3"
-timeout "${time_limit}" time -ap -o "${output_log}" python3 ../cdcl_tlra_solver.py "${benchmark}" 2>&1 | tee "${output_log}"
+solver="${@:4}"
+timeout "${time_limit}" time -ap -o "${output_log}" ${solver} "${benchmark}" 2>&1 | tee "${output_log}"
 status="${PIPESTATUS[0]}"
 if [ "${status}" = 124 ]
 then
