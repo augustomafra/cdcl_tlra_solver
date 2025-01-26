0c3d8c8bbdcb94cfe2548f277e1cdcaee434ff42
Augusto Mafra <augustomafra1@gmail.com>
2025-01-26 16:32:39 -0300

    Save statistics.txt file in summary directory

--- benchmark/run_summary.sh
+++ benchmark/run_summary.sh
@@ -1,73 +1,77 @@
 #!/bin/bash
-echo "### Benchmark Statistics ###"
-echo ""
+mkdir -p summary
+
+echo "### Benchmark Statistics ###" > summary/statistics.txt
+echo "" >> summary/statistics.txt
 
 test_results=`find . -name '*.log'`
 total=`wc -w <<< ${test_results}`
 
-echo "Number of tests run: ${total}"
-echo ""
+echo "Number of tests run: ${total}" >> summary/statistics.txt
+echo "" >> summary/statistics.txt
 
 if [ "${total}" = 0 ]
 then
+    cat summary/statistics.txt
     exit
 fi
 
-mkdir -p summary
 
 failures=`grep -lw '^error:\|^Traceback' ${test_results}`
 error_count=`wc -w <<< ${failures}`
 successes=`grep -Lw '^error:\|^Traceback' ${test_results}`
 success_count=`wc -w <<< ${successes}`
 
-echo "Successes: ${success_count}"
+echo "Successes: ${success_count}" >> summary/statistics.txt
 if [ "${success_count}" != 0 ]
 then
 
     sat=`grep -lw '^sat' ${successes}`
     sat_count=`wc -w <<< ${sat}`
-    echo "    sat: ${sat_count}"
+    echo "    sat: ${sat_count}" >> summary/statistics.txt
     echo "${sat}" > summary/sat.txt
 
     unsat=`grep -lw '^unsat' ${successes}`
     unsat_count=`wc -w <<< ${unsat}`
-    echo "    unsat: ${unsat_count}"
+    echo "    unsat: ${unsat_count}" >> summary/statistics.txt
     echo "${unsat}" > summary/unsat.txt
 
     unknown=`grep -lw '^unknown' ${successes}`
     unknown_count=`wc -w <<< ${unknown}`
-    echo "    unknown: ${unknown_count}"
+    echo "    unknown: ${unknown_count}" >> summary/statistics.txt
     echo "${unknown}" > summary/unknown.txt
-    echo ""
+    echo "" >> summary/statistics.txt
 fi
 
-echo "Failures: ${error_count}"
+echo "Failures: ${error_count}" >> summary/statistics.txt
 if [ "${error_count}" = 0 ]
 then
+    cat summary/statistics.txt
     exit
 fi
 
 soundness=`grep -lw '^error: expected result was' ${failures}`
 soundess_count=`wc -w <<< ${soundness}`
-echo "    soundness: ${soundess_count}"
+echo "    soundness: ${soundess_count}" >> summary/statistics.txt
 echo "${soundness}" > summary/soundness.txt
 
 timeouts=`grep -lw '^error: timed out after' ${failures}`
 timeout_count=`wc -w <<< ${timeouts}`
-echo "    timeout: ${timeout_count}"
+echo "    timeout: ${timeout_count}" >> summary/statistics.txt
 echo "${timeouts}" > summary/timeout.txt
 
 stackoverflow=`grep -lw '^error: maximum recursion depth exceeded' ${failures}`
 so_count=`wc -w <<< ${stackoverflow}`
-echo "    stack overflow: ${so_count}"
+echo "    stack overflow: ${so_count}" >> summary/statistics.txt
 echo "${stackoverflow}" > summary/stackoverflow.txt
 
 exceptions=`grep -lw '^Traceback' ${failures}`
 exception_count=`wc -w <<< ${exceptions}`
-echo "    python exception: ${exception_count}"
+echo "    python exception: ${exception_count}" >> summary/statistics.txt
 
 if [ "${exception_count}" = 0 ]
 then
+    cat summary/statistics.txt
     exit
 fi
 
@@ -80,7 +84,8 @@ for type in ${exception_types}
 do
     type_list=`grep -lw "^${type}" ${exceptions}`
     type_count=`wc -w <<< ${type_list}`
-    echo "        ${type} ${type_count}"
+    echo "        ${type} ${type_count}" >> summary/statistics.txt
     echo "${type_list}" > summary/"${type//[:.]/}.txt"
 done
 
+cat summary/statistics.txt
